2aef4e08339276ca4fcbe5d37b034334da021d14
whysofast <matheusvca@gmail.com>
2021-08-22 02:09:19 -0300

    first commit

---
+++ src/main/kotlin/com/ccca/commerce/CommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.ccca.commerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<CommerceApplication>(*args)
+}
---
+++ src/test/kotlin/com/ccca/commerce/CommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ccca.commerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
