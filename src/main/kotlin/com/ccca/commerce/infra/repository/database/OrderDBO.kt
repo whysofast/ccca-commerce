face9b4f9eba76718a2b0fb4365f9f43236dd625
WhySoFast <matheusvca@gmail.com>
2021-09-07 05:10:45 -0300

    Implemeta OneToMany para orderItem e setup h2 in memory

--- src/main/kotlin/com/ccca/commerce/infra/repository/database/OrderDBO.kt
+++ src/main/kotlin/com/ccca/commerce/infra/repository/database/OrderDBO.kt
@@ -2,14 +2,17 @@ package com.ccca.commerce.infra.repository.database
 
 import com.ccca.commerce.domain.entity.Cpf
 import com.ccca.commerce.domain.entity.Order
-import org.hibernate.annotations.GenericGenerator
+import org.hibernate.annotations.LazyCollection
+import org.hibernate.annotations.LazyCollectionOption
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import java.time.LocalDate
 import java.time.LocalDateTime
+import javax.persistence.CascadeType
 import javax.persistence.Column
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 import javax.persistence.Id
 import javax.persistence.JoinColumn
 import javax.persistence.ManyToOne
@@ -18,9 +21,8 @@ import javax.persistence.OneToMany
 @Entity(name = "pedido")
 class OrderDBO(
     @Id
-    @GeneratedValue(generator = "uuid2")
-    @GenericGenerator(name = "uuid2", strategy = "uuid2")
-    val id: String? = null,
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    val id: Long? = null,
 
     val cpf: String,
 
@@ -28,10 +30,12 @@ class OrderDBO(
     @JoinColumn(name = "name")
     val coupon: CouponDBO? = null,
 
-    @OneToMany
-    val items: MutableList<OrderItemDBO> = mutableListOf(),
-
     val shippingPrice: Double = 0.0,
+
+    @OneToMany(cascade = [CascadeType.ALL], orphanRemoval = true)
+    @JoinColumn(name = "pedido_id")
+    @LazyCollection(LazyCollectionOption.FALSE)
+    val items: MutableList<OrderItemDBO> = mutableListOf(),
 
     val issueDate: LocalDate = LocalDate.now(),
 
