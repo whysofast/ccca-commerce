39ec3bcb04e999eafa6e3843d3325dbfc9e4e2df
WhySoFast <matheusvca@gmail.com>
2021-09-10 01:39:53 -0300

    Refactor removendo implementação do calculate usando Item

--- src/main/kotlin/com/ccca/commerce/domain/service/TaxCalculator.kt
+++ src/main/kotlin/com/ccca/commerce/domain/service/TaxCalculator.kt
@@ -1,19 +1,12 @@
 package com.ccca.commerce.domain.service
 
-import com.ccca.commerce.domain.entity.Item
 import com.ccca.commerce.domain.entity.OrderItem
 import com.ccca.commerce.domain.entity.TaxTable
 import org.springframework.stereotype.Component
 
 @Component
 abstract class TaxCalculator {
-
-    fun calculate(item: Item, taxTables: List<TaxTable?>): Double {
-
-        return ((item.price * getTax(taxTables)) / 100)
-
-    }
-
+    
     fun calculate(orderItem: OrderItem, taxTables: List<TaxTable?>): Double {
 
         return ((orderItem.price * getTax(taxTables)) / 100)
--- src/test/kotlin/com/ccca/commerce/integration/TaxCalculatorTest.kt
+++ src/test/kotlin/com/ccca/commerce/integration/TaxCalculatorTest.kt
@@ -1,6 +1,6 @@
 package com.ccca.commerce.integration
 
-import com.ccca.commerce.domain.entity.Item
+import com.ccca.commerce.domain.entity.OrderItem
 import com.ccca.commerce.domain.entity.TaxTable
 import com.ccca.commerce.domain.factory.TaxCalculatorFactory
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -13,52 +13,52 @@ class TaxCalculatorTest() {
 
     @Test
     fun `should calculate a Guitarra item tax for any month but november`() {
-        val item = Item("1", "Guitarra", 1000, 100, 50, 30, 8)
+        val orderItem = OrderItem("1", 1000, 2)
 
         val taxTables = listOf(TaxTable("1", "default", 15.0), TaxTable("1", "november", 5.0))
 
         val taxCalculator = TaxCalculatorFactory().create(LocalDate.of(2021, 5, 10))
 
-        val taxAmount = taxCalculator.calculate(item, taxTables)
+        val taxAmount = taxCalculator.calculate(orderItem, taxTables)
 
         assertEquals(150.0, taxAmount)
     }
 
     @Test
     fun `should calculate a Guitarra item tax for november`() {
-        val item = Item("1", "Guitarra", 1000, 100, 50, 30, 8)
+        val orderItem = OrderItem("1", 1000, 2)
 
         val taxTables = listOf(TaxTable("1", "default", 15.0), TaxTable("1", "november", 5.0))
 
         val taxCalculator = TaxCalculatorFactory().create(LocalDate.of(2021, 11, 10))
 
-        val taxAmount = taxCalculator.calculate(item, taxTables)
+        val taxAmount = taxCalculator.calculate(orderItem, taxTables)
 
         assertEquals(50.0, taxAmount)
     }
 
     @Test
     fun `should calculate a Cabo item tax for any month but november`() {
-        val item = Item("2", "Cabo", 30, 10, 10, 10, 1)
+        val orderItem = OrderItem("2", 30, 2)
 
         val taxTables = listOf(TaxTable("3", "default", 5.0), TaxTable("2", "november", 1.0))
 
         val taxCalculator = TaxCalculatorFactory().create(LocalDate.of(2021, 5, 10))
 
-        val taxAmount = taxCalculator.calculate(item, taxTables)
+        val taxAmount = taxCalculator.calculate(orderItem, taxTables)
 
         assertEquals(1.5, taxAmount)
     }
 
     @Test
     fun `should calculate a Cabo item tax for november`() {
-        val item = Item("2", "Cabo", 30, 10, 10, 10, 1)
+        val orderItem = OrderItem("2", 30, 2)
 
         val taxTables = listOf(TaxTable("3", "default", 5.0), TaxTable("2", "november", 1.0))
 
         val taxCalculator = TaxCalculatorFactory().create(LocalDate.of(2021, 11, 10))
 
-        val taxAmount = taxCalculator.calculate(item, taxTables)
+        val taxAmount = taxCalculator.calculate(orderItem, taxTables)
 
         assertEquals(0.3, taxAmount)
     }
